20370df0704b6c9e546d6fc95ad4fcae09dea49c
Victor Emanoel <victoremanoelmoraesamaral@gmail.com>
2026-08-01 18:07:52 -0400

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -51,4 +51,4 @@ Refresh-Env
 sdkmanager "platform-tools" "platforms;android-36" "ndk;27.3.13750724" "build-tools;36.0.0" "cmdline-tools;22.0"
 
 # Install Microsoft C++ Build Tools
-choco install visualstudio2022buildtools --package-parameters "--add Microsoft.VisualStudio.Workload.VCTools --includeRecommended --quiet --wait" --yes --limit-output
+choco install visualstudio2022buildtools --version=117.14.37 --package-parameters "--add Microsoft.VisualStudio.Workload.VCTools --includeRecommended --quiet --wait" --yes --limit-output
