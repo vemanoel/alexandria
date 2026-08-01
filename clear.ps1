48c13840fb318c8e067e1cd45f4f584fb130f577
Victor Emanoel <victoremanoelmoraesamaral@gmail.com>
2026-08-01 18:01:01 -0400

    Update clear.ps1

--- clear.ps1
+++ clear.ps1
@@ -6,7 +6,9 @@ choco uninstall rust --yes
 choco uninstall android-sdk --yes
 choco uninstall visualstudio2022buildtools --yes
 
+# Uninstall Chocolatey
 Remove-Item C:\ProgramData\chocolatey -Recurse -Force
+Remove-Item C:\ProgramData\ChocolateyHttpCache -Recurse -Force
 
 [Environment]::SetEnvironmentVariable(
     "Path",
@@ -14,3 +16,7 @@ Remove-Item C:\ProgramData\chocolatey -Recurse -Force
     Where-Object { $_ -notlike "*chocolatey*" }) -join ";",
     "Machine"
 )
+
+# Uninstall Android Studio
+Remove-Item C:\Android -Recurse -Force
+Remove-Item "$env:USERPROFILE\.android" -Recurse -Force
