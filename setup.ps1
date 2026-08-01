98c5205eb6f69c0bbe761bbe65bfbb5126534b7c
Victor Emanoel <victoremanoelmoraesamaral@gmail.com>
2026-08-01 18:29:36 -0400

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -3,7 +3,7 @@
 $ErrorActionPreference = "Stop"
 
 # Install Chocolatey
-Remove-Dir "C:\ProgramData\chocolatey"
+Remove-Dir "$env:ProgramData\ProgramData\chocolatey"
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
