48740bf7415bb07b1ad97f37d8cde5a864196d02
Victor Emanoel <victoremanoelmoraesamaral@gmail.com>
2026-08-01 18:34:29 -0400

    Update utils.ps1

--- utils.ps1
+++ utils.ps1
@@ -1,11 +1,1 @@
-function Remove-Dir {
-    param(
-        [Parameter(Mandatory=$true)]
-        [string]$Path
-    )
-
-    if (Test-Path $Path) {
-        Remove-Item $Path -Recurse -Force
-    }
-}
 
