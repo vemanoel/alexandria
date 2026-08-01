8d2c2cd715288f915e2a2263c5b6f13bbd443be9
Victor Emanoel <victoremanoelmoraesamaral@gmail.com>
2026-08-01 19:51:37 -0400

    Update clear.ps1

--- clear.ps1
+++ clear.ps1
@@ -1,39 +1,5 @@
 $ErrorActionPreference = "Stop"
 
-function Remove-Dir {
-    param(
-        [Parameter(Mandatory=$true)]
-        [string]$Path
-    )
-
-    if (Test-Path $Path) {
-        Remove-Item $Path -Recurse -Force
-    }
-}
-
-function Remove-PathEntry {
-    param(
-        [Parameter(Mandatory)]
-        [string]$Pattern,
-
-        [ValidateSet("Machine", "User")]
-        [string]$Scope = "Machine"
-    )
-
-    $path = [Environment]::GetEnvironmentVariable("Path", $Scope)
-
-    $newPath = ($path -split ";" |
-        Where-Object {
-            $_ -and $_ -notlike "*$Pattern*"
-        }) -join ";"
-
-    [Environment]::SetEnvironmentVariable(
-        "Path",
-        $newPath,
-        $Scope
-    )
-}
-
 choco uninstall just --yes --remove-dependencies
 choco uninstall git --yes --remove-dependencies --params="'/VERYSILENT /NORESTART'"
 choco uninstall nodejs --yes --remove-dependencies
@@ -42,18 +8,23 @@ choco uninstall rustup --yes --remove-dependencies
 choco uninstall android-sdk --yes --remove-dependencies
 choco uninstall visualstudio2022buildtools --yes --remove-dependencies
 
-Remove-Dir "C:\Android"
-Remove-Dir "$env:TEMP\pnpm"
-Remove-Dir "$env:TEMP\pnpm-cache"
-Remove-Dir "$env:TEMP\pnpm-state"
-Remove-Dir "$env:TEMP\.tauri"
-Remove-Dir "$env:ProgramFiles\Git"
-Remove-Dir "$env:ProgramFiles\nodejs"
-Remove-Dir "$env:ProgramFiles\Java"
-Remove-Dir "$env:USERPROFILE\.cargo"
-Remove-Dir "$env:USERPROFILE\.android"
-Remove-Dir "$env:TEMP\chocolatey"
-Remove-Dir "$env:ProgramData\chocolatey"
-Remove-Dir "$env:ProgramData\ChocolateyHttpCache"
+Remove-Item "C:\Android" -Recurse -Force
+Remove-Item "$env:TEMP\pnpm" -Recurse -Force
+Remove-Item "$env:TEMP\pnpm-cache" -Recurse -Force
+Remove-Item "$env:TEMP\pnpm-state" -Recurse -Force
+Remove-Item "$env:TEMP\.tauri" -Recurse -Force
+Remove-Item "$env:ProgramFiles\Git" -Recurse -Force
+Remove-Item "$env:ProgramFiles\nodejs" -Recurse -Force
+Remove-Item "$env:ProgramFiles\Java" -Recurse -Force
+Remove-Item "$env:USERPROFILE\.cargo" -Recurse -Force
+Remove-Item "$env:USERPROFILE\.android" -Recurse -Force
+Remove-Item "$env:TEMP\chocolatey" -Recurse -Force
+Remove-Item "$env:ProgramData\chocolatey" -Recurse -Force
+Remove-Item "$env:ProgramData\ChocolateyHttpCache" -Recurse -Force
 
-Remove-PathEntry "chocolatey"
+[Environment]::SetEnvironmentVariable(
+    "Path",
+    ([Environment]::GetEnvironmentVariable("Path", "Machine") -split ";" |
+        Where-Object { $_ -and $_ -notlike "*chocolatey*" }) -join ";",
+    "Machine"
+)
