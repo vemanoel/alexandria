de42168ccd83020d43645795c31fb6249eab5a56
Victor Emanoel <victoremanoelmoraesamaral@gmail.com>
2026-08-01 22:22:58 -0400

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -66,5 +66,4 @@ sdkmanager `
 winget install `
     --id Microsoft.VisualStudio.2022.BuildTools `
     --version 17.14.37 `
-    --silent `
     --override "--add Microsoft.VisualStudio.Workload.VCTools --includeRecommended --quiet --wait"
