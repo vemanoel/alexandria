5e7506a4e43c935b368d2653be561f510f091e02
Victor Emanoel <victoremanoelmoraesamaral@gmail.com>
2026-08-01 22:22:22 -0400

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,16 +1,5 @@
 $ErrorActionPreference = "Stop"
 
-function Remove-Dir {
-    param(
-        [Parameter(Mandatory=$true)]
-        [string]$Path
-    )
-
-    if (Test-Path $Path) {
-        Remove-Item $Path -Recurse -Force
-    }
-}
-
 function Refresh-Env {
     [Environment]::GetEnvironmentVariables("Machine").GetEnumerator() | ForEach-Object {
         Set-Item -Path "Env:$($_.Key)" -Value $_.Value
@@ -26,38 +15,56 @@ function Refresh-Env {
     ) -join ";"
 }
 
-# Install Chocolatey
-Remove-Dir "$env:ProgramData\chocolatey"
-Remove-Dir "$env:ProgramData\ChocolateyHttpCache"
-Set-ExecutionPolicy Bypass -Scope Process -Force
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+# Install Scoop
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 Refresh-Env
+
+# Install 7zip
+scoop install 7zip
 
 # Install Just
-choco install just --version="1.57.0" --yes --limit-output --failonstderr --force
-choco pin add -n=just
+scoop install just@1.57.0
+scoop hold just
 
 # Install Git
-choco install git --version="2.55.0" --yes --limit-output --failonstderr --force
-choco pin add -n=git
+scoop install git@2.55.0.3
+scoop hold git
 
 # Install Node
-choco install nodejs --version="24.18.1" --yes --limit-output --failonstderr --force
-choco pin add -n=nodejs
+scoop install nodejs@24.18.1
+scoop hold nodejs
 
 # Install PNPM
-choco install pnpm --version="11.18.0" --yes --limit-output --failonstderr --force
-choco pin add -n=pnpm
+scoop install pnpm@11.18.0
+scoop hold pnpm
 
 # Install Rust
-choco install rust --version="1.97.0" --yes --limit-output --failonstderr --force
-choco pin add -n=rust
-     
+scoop install rustup@1.29.0
+scoop hold rustup
+
+# Install JDK
+scoop bucket add java
+scoop update java
+scoop install temurin21-jdk@21.0.12-8.0
+scoop hold temurin21-jdk
+
 # Install Android SDK Command-line Tool
-choco install android-sdk --version="26.1.1" --yes --limit-output --failonstderr --force
+scoop install android-clt@15859902
+scoop hold android-clt
 Refresh-Env
-sdkmanager "platform-tools" "platforms;android-36" "ndk;27.3.13750724" "build-tools;36.0.0" "cmdline-tools;22.0"
+
+# Install Android SDK components
+sdkmanager `
+    "platform-tools" `
+    "platforms;android-36" `
+    "ndk;27.3.13750724" `
+    "build-tools;36.0.0" `
+    "cmdline-tools;22.0"
 
 # Install Microsoft C++ Build Tools
-choco install visualstudio2022buildtools --version=117.14.37 --package-parameters "--add Microsoft.VisualStudio.Workload.VCTools --includeRecommended --quiet --wait" --yes --limit-output --failonstderr --force
+winget install `
+    --id Microsoft.VisualStudio.2022.BuildTools `
+    --version 17.14.37 `
+    --silent `
+    --override "--add Microsoft.VisualStudio.Workload.VCTools --includeRecommended --quiet --wait"
